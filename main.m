bf2f5ad25506cd76160a1c76aaed6a1ee105cfd1
Rajasekhar2001 <57327711+Rajasekhar2001@users.noreply.github.com>
2020-12-13 12:54:46 +0530

    Add files via upload

---
+++ main.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+close all;
+disp('FSK without noise:');
+FSK%FSK signal without noise
+disp('FSK with noise');
+FSK_noise%FSK signal with noise
+disp("ASK without noise");
+ASK %ASK signal without noise
+disp('ASK with noise');
+ASK_noise% ASK signal with noise
+disp("BPSK without noise");
+BPSK%BPSK signal without noise
+disp("BPSK without noise");
+BPSK_noise%BPSK signal with noise
+disp("QPSK");
+QPSK%QPSK signal
